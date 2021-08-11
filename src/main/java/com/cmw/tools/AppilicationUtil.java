6bd33288a920785532e0856a489f3c4ada6a7541
陈明伟 <chenmingwei@qinjia-tech.com>
2021-08-11 15:35:44 +0800

    1

---
+++ src/main/java/com/cmw/tools/AppilicationUtil.java
@@ -0,0 +1,48 @@
+package com.cmw.tools;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @description:  当一个类实现了这个接口（ApplicationContextAware）之后，
+ * 这个类就可以方便获得ApplicationContext中的所有bean。换句话说，就是这个类可以直接获取spring配置文件中，所有有引用到的bean对象
+ * @author: cmw
+ * @data: 2021/8/11
+ */
+public class AppilicationUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+    public ApplicationContext getApplicationContext(){
+        return applicationContext;
+    }
+
+    /**
+     * 获取对象
+     *
+     * @param name spring配置文件中配置的bean名或注解的名称
+     * @return 一个以所给名字注册的bean的实例
+     * @throws BeansException 抛出spring异常
+     */
+    public static <T> T getBeanByname(String name){
+        return (T)applicationContext.getBean(name);
+    }
+
+    /**
+     * 获取类型为requiredType的对象
+     *
+     * @param clazz 需要获取的bean的类型
+     * @return 该类型的一个在ioc容器中的bean
+     * @throws BeansException 抛出spring异常
+     */
+    public static <T> T getBean(Class<T> clazz) throws BeansException {
+        return applicationContext.getBean(clazz);
+    }
+}
